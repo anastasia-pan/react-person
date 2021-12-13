93ac83a8e897b4e5f9e6245b4a70b921a8ff6de3
Anastasia Panoutsou <panoutsou.anastasia@gmail.com>
2021-12-13 14:56:12 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello from app </h1>
+      <Person
+        name="Dan"
+        age="34"
+        job="junior developer"
+        petname="hayley"
+        type="cat"
+      />
+
+      <Person name="Anastasia" age="36" />
     </div>
   );
-}
+};
+
+const Person = ({ name, age, job, petname, type }) => {
+  return (
+    <div>
+      <h2>
+        Hello my name is {name} and I'm {age}
+      </h2>
+      <Job job={job} />
+      <Pet petname={petname} type={type} />
+    </div>
+  );
+};
+
+const Job = (props) => {
+  return <h2>My job is {props.job}</h2>;
+};
+
+const Pet = (props) => {
+  return (
+    <h2>
+      My pet's name is {props.petname}, it's a {props.type}
+    </h2>
+  );
+};
 
 export default App;
